ba5da7a5a48825af766adfb1105cba264f367937
Ko Takagi <knockoutmarch@gmail.com>
2021-10-22 19:48:30 +0900

    :+1: Add feed item tags automatically

--- plugins/feed-collector.php
+++ plugins/feed-collector.php
@@ -56,3 +56,50 @@ add_filter( 'fc_feed_item_category_register_taxonomy_args', function ( $args ) {
 
 	return $args;
 } );
+
+/**
+ * When new feed item saves, analyze specific nouns from the post_title, and save them to term.
+ *
+ * @param int $post_ID Post ID.
+ * @param WP_Post $post Post object.
+ * @param bool $update Whether this is an existing post being updated.
+ */
+add_action( 'wp_insert_post', function ( $post_ID, $post, $update ) {
+	if ( wp_is_post_revision( $post_ID ) ) {
+		return;
+	}
+
+	if ( $post->post_type !== 'fc-feed-item' ) {
+		return;
+	}
+
+	$endpoint_url = 'https://labs.goo.ne.jp/api/entity';
+
+	// Get application ID from option data.
+	$app_id = get_option( 'goo_entity_app_id' );
+	if ( empty( $app_id ) ) {
+		return;
+	}
+
+	$args     = [
+		'body' => [
+			'app_id'   => $app_id,
+			'sentence' => $post->post_title,
+		],
+	];
+	$response = wp_remote_post( $endpoint_url, $args );
+	if ( is_wp_error( $response ) ) {
+		return;
+	}
+
+	$data = json_decode( $response['body'], true );
+	if ( is_array( $data['ne_list'] ) ) {
+		$terms = [];
+		foreach ( $data['ne_list'] as $item ) {
+			$terms[] = (string) $item[0];
+		}
+		if ( $terms ) {
+			wp_set_object_terms( $post_ID, $terms, 'fc-feed-item-tag' );
+		}
+	}
+}, 10, 3 );
